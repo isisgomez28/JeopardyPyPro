2424dba01a991473ac4790d7e855dfe9a76b67f1
isisgomez28 <igomezp.28@gmail.com>
2015-07-18 14:34:16 -0400

    Iniciando con la Base de Datos del juego.

---
+++ pl/GameDatabase.pl
@@ -0,0 +1,30 @@
+%	Categorias
+categoria("Bandas Sonoras", "Musica en las peliculas.").
+categoria("Actores y Hechos", "Actores fuera de las peliculas.").
+%% categoria("","").
+%% categoria("", "").
+%% categoria("", "").
+
+% Preguntas.
+%% Preguntas relacionadas a la categoría de Bandas Sonoras.
+pregunta(1, "Es un compositor famoso de las peliculas Star Wars, Harry Potter y Jaws", "Bandas Sonoras", 100).
+pregunta(2, "Cancion utilizada por Rocky Balboa en sus Entrenamientos en la saga completa", "Bandas Sonoras", 200).
+pregunta(3, "Es el compositor que gano mejor cancion en los Oscar en el 2015", "Bandas Sonoras", 300).
+pregunta(4, "Canción utilizada en Inception para despertar a los personajes", "Bandas Sonoras", 400).
+pregunta(5, "Efecto de sonido de stock usado por primera vez en 1951 en la pelicula Distant Drums", "Bandas Sonoras", 500).
+
+%% Preguntas relacionadas a la categoría de Actores y Hechos.
+pregunta(6, "Es el actor que rechazo interpretar el papel de Neo en la trilogia de Matrix").
+pregunta(7, "Fue el actor que interpreto el personaje de Oscar Schindler en el drama de WWII Schindler's List").
+pregunta().
+
+% Respuesta.
+respuesta(1, "Quien es John Williams").
+respuesta(2, "Gonna Fly Now").
+respuesta(3, "Quien es John Legend").
+respuesta(4, "Non Je Ne Regrette Rien").
+respuesta(5, "Que es el El grito de Wilhem").
+respuesta(6, "Quien es Will Smith").
+respuesta(7, "Quien es Liam Neeson").
+
+
