992048f3082b467981c070aaa60fe931c67a3755
isisgomez28 <igomezp.28@gmail.com>
2015-07-18 16:03:37 -0400

    Working on it
    Correcciones

--- pl/GameDatabase.pl
+++ pl/GameDatabase.pl
@@ -1,6 +1,6 @@
 %	Categorias
 categoria("Bandas Sonoras", "Musica en las peliculas.").
-categoria("Actores y Hechos", "Actores fuera de las peliculas.").
+categoria("Actores y Hechos Curiosos", "Actores fuera de las peliculas.").
 %% categoria("","").
 %% categoria("", "").
 %% categoria("", "").
@@ -13,9 +13,14 @@ pregunta(3, "Es el compositor que gano mejor cancion en los Oscar en el 2015", "
 pregunta(4, "Canción utilizada en Inception para despertar a los personajes", "Bandas Sonoras", 400).
 pregunta(5, "Efecto de sonido de stock usado por primera vez en 1951 en la pelicula Distant Drums", "Bandas Sonoras", 500).
 
-% Preguntas relacionadas a la categoría de Actores y Hechos.
-pregunta(6, "Es el actor que rechazo interpretar el papel de Neo en la trilogia de Matrix").
-pregunta(7, "Fue el actor que interpreto el personaje de Oscar Schindler en el drama de WWII Schindler's List").
+% Preguntas relacionadas a la categoría de Actores y Hechos Curiosos.
+pregunta(6, "Enfermedad que arruino su carrera de actor a Charlie Sheen", "Actores y Hechos Curiosos", 100).
+pregunta(7, "Es el actor que rechazo interpretar el papel de Neo en la trilogia de Matrix", "Actores y Hechos Curiosos", 200).
+pregunta(8, "Fue el superheroe por el cual Riggan Thomson (Michael Keaton) tuvo delirios durante su vida profesional.", "Actores y Hechos Curiosos" ,300).
+pregunta(9, "", "", 400).
+pregunta(10, "", "", 500).
+
+% Preguntas
 
 % Respuesta.
 respuesta(1, "Quien es John Williams").
@@ -23,7 +28,9 @@ respuesta(2, "Gonna Fly Now").
 respuesta(3, "Quien es John Legend").
 respuesta(4, "Non Je Ne Regrette Rien").
 respuesta(5, "Que es el El grito de Wilhem").
-respuesta(6, "Quien es Will Smith").
-respuesta(7, "Quien es Liam Neeson").
-
+respuesta(6, "Alcoholismo").
+respuesta(7, "Quien es Will Smith").
+respuesta(8, "Quien es Birdman").
+respuesta(9, "").
+respuesta(10, "").
 
