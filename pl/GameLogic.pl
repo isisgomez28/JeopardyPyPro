beee8c4fd79f9f10bec8992cca038c951331e28b
isisgomez28 <igomezp.28@gmail.com>
2015-07-19 14:51:24 -0400

    Regla para ganador

--- pl/GameLogic.pl
+++ pl/GameLogic.pl
@@ -12,5 +12,9 @@ nuevo_jugador(Nombre):- assertz(jugador(Nombre)), assertz(puntuacion(Nombre, 0))
 %% Regla para la Actualizacion de la Puntacion del Jugador.
 actualiza_puntuacion(Jug, PtosNuevos):- number(PtosNuevos), retract(puntuacion(Jug, X)), Y is (X + PtosNuevos), assertz(puntuacion(Jug, Y)).
 
+%% Ganador
+ganador(Jug1, Jug2, Ganador):- puntuacion(Jug1, X), puntuacion(Jug2, Y), auxiliarGanador(X, Y, Ganador, Jug1, Jug2).
+auxiliarGanador(X, Y, Ganador, Jug1, Jug2) :-  X > Y, Ganador = Jug1 ; X < Y, Ganador = Jug2; X==Y, Ganador = 'Empate'.
+
 %% Regla de Limpieza de Memoria.
 borra_memoria():- retractall(puntuacion(_, _)), retractall(jugador(_)).
